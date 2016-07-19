3e9630af660bee64c6f6c93e65949969ba3b7fcd
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-07-19 12:57:33 +0300

    Added function to return the index of the list when choosing p parametet

--- lab_16.py
+++ lab_16.py
@@ -2,10 +2,17 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+def ComboBoxChoice(combobox):
+	print('Онотоле — краб!')
+	txt = combobox.get_active()
+	print(txt)
+
+
 builder = Gtk.Builder()
 builder.add_from_file('lab_step1.glade')
 handlers = {
-	'OnDeleteWindow': Gtk.main_quit
+	'OnDeleteWindow': Gtk.main_quit,
+	'OnChange': ComboBoxChoice
 }
 
 builder.connect_signals(handlers)

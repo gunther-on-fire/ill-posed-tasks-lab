2deb7aeff95260fc60708b612beaa97fce0b19f4
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-07-19 17:36:29 +0300

    Added print function to X length entry

--- lab_16.py
+++ lab_16.py
@@ -3,15 +3,21 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 def ComboBoxChoice(combobox):
-	#import ipdb; ipdb.set_trace()
-	txt = combobox.get_model()[combobox.get_active()]
-	print(txt[0])
+    #import ipdb; ipdb.set_trace()
+    txt = combobox.get_model()[combobox.get_active()]
+    print(txt[0])
+
+def ChangedEntry(button):
+    entry = builder.get_object('X length_entry')
+    number = int(entry.get_text())
+    print(number)
 
 builder = Gtk.Builder()
 builder.add_from_file('lab_step1.glade')
 handlers = {
-	'OnDeleteWindow': Gtk.main_quit,
-	'OnChange': ComboBoxChoice
+    'OnDeleteWindow': Gtk.main_quit,
+    'OnChange': ComboBoxChoice,
+    'OnEntryChange': ChangedEntry
 }
 
 builder.connect_signals(handlers)

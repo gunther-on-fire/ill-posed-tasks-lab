4b4f4023201743512f1135541ced4833d4fabf73
Guntherswarchzhaffenstrassen <boris@localhost.localdomain>
2016-10-04 14:52:01 +0300

    Entry value in tab5 is shown

--- handlers/tab5.py
+++ handlers/tab5.py
@@ -14,6 +14,8 @@ class Handler:
     def updateOutSigPlot(self, button):
 
         self.app.output_signal.cla()
+        self.app.noise_to_signal_entry = self.app.builder.get_object('signal_noise_ratio_entry')
+        self.app.noise_to_signal_entry.set_text('0')  
 
         self.app.fft_output_y = self.app.norm_ampl_fft_fwhl_y*self.app.fft_input_y
 
@@ -24,12 +26,12 @@ class Handler:
 
         # Calculating noise/signal ratio
 
-        self.app.mean_output_y = sum(self.app.output_y)/self.app.non_discrete_input_x
+        self.app.mean_output_y = sum(self.app.output_y)/len(self.app.non_discrete_input_x)
+        
         self.app.mean_noise = self.app.builder.get_object('mean_poisson')
-
         self.app.noise_to_signal_ratio = self.app.mean_output_y/float(self.app.mean_noise.get_value())
+        
 
-        # TOFIX: set the entry value -- doesn't work
+        #TOFIX: Entry update doesn't work
         self.app.noise_to_signal_entry = self.app.builder.get_object('signal_noise_ratio_entry')
-        self.app.noise_to_signal_entry.set_text(str(self.app.noise_to_signal_ratio))              
-
+        self.app.noise_to_signal_entry.set_text(str(self.app.noise_to_signal_ratio))
